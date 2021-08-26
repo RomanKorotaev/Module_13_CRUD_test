9cd21d349021e0349869f376571b5be64424c7c8
Roman Korotaev <kro1986@ukr.net>
2021-08-26 14:30:34 +0300

    01

---
+++ server.js
@@ -0,0 +1,21 @@
+//console.log('Hello, World!')
+
+// const Joi = require('joi');
+
+// const passwordSchema = Joi.string().min(3).max(10).alphanum();
+
+// console.log(passwordSchema.validate ('qwer'))
+
+
+const express = require('express');
+
+const app = express();
+
+app.use('*', (req, res) => {
+    console.log("Был запрос от броузера");
+    res.send("<h1>Ну это мол HTML тебе в ответ</h1>")
+ })
+
+const listener = app.listen(4444, () => {
+    console.log(`Веб-сервер запущен на порте ${listener.address().port}`)
+});
