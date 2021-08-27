062e629845d8693d3787d847ad24c8e97dd99f2b
Roman Korotaev <kro1986@ukr.net>
2021-08-27 13:15:59 +0300

    Протестирован post-запрос

--- js/c - post.js
+++ js/c - post.js
@@ -1,7 +1,51 @@
 const BASE_URL = 'http://localhost:4040';
 
-const options = {};
+const newBook = {
+    
+    "title": "Тестовая книга по CSS",
+    "author": "Я",
+    "genres": [ "CSS" ],
+    "rating": 9,
+};
 
-// function fetchBooks() {
-//     return fetch(`${BASE_URL}/books`).then(res => res.json());
-//  }
+const options = {
+    method: 'POST',
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify(newBook),
+};
+
+
+function addBook(book) {
+
+    const options = {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(book),
+    };
+
+    return fetch(`${BASE_URL}/books`, options).then(res => res.json());
+
+ }
+
+ addBook ({
+    "title": "Тестовая книга по HTML",
+    "author": "Я",
+    "genres": [ "HTML" ],
+    "rating": 8,
+ }).then(renderBook)
+
+  addBook ({
+    "title": "Тестовая книга по C++",
+    "author": "Я",
+    "genres": [ "C++" ],
+    "rating": 7.7,
+}).then(renderBook)
+ 
+function renderBook(book) {
+    console.log('Пришёл ответ от бекенда, можно рисовать');
+     console.log(book);
+}
