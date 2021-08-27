2dd098b8fd50f37b3b38fddbcfb3d9cdd7745e0c
Roman Korotaev <kro1986@ukr.net>
2021-08-27 12:49:17 +0300

    протестирован get-запрос

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log (' This is file  index.js ')
+console.log(' This is file  index.js ');
+
+//import './js/r - get';
+
+//import './js/d - delete';
+
+import './js/c - post';
+
+//import './js/u - patch';
---
+++ js/c - post.js
@@ -0,0 +1,7 @@
+const BASE_URL = 'http://localhost:4040';
+
+const options = {};
+
+// function fetchBooks() {
+//     return fetch(`${BASE_URL}/books`).then(res => res.json());
+//  }
